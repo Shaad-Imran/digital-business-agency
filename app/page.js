6b5a7679ec9a18368ec7ec80634380f5edd32252
Imran <imranmansuri038@gmail.com>
2023-09-06 13:36:35 +0530

    Added a responsive Navbar component

--- app/page.js
+++ app/page.js
@@ -1,3 +1,10 @@
+import Navbar from "@/src/components/Navbar/Navbar";
+import "./page.css";
+
 export default function Home() {
-  return <div>Welcome to NEXT</div>;
+  return (
+    <div className="app">
+      <Navbar />
+    </div>
+  );
 }
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,64 @@
+"use client";
+
+import React, { useState } from "react";
+import "./Navbar.css";
+import { BiMenuAltRight } from "react-icons/bi";
+import { RxCross2 } from "react-icons/rx";
+
+const Navbar = () => {
+  const [mobileMenuOpened, setMobileMenuOpened] = useState(false);
+
+  return (
+    <div className="n-wrapper">
+      {/* desktop version  */}
+      <div className="container">
+        <div className="n-container">
+          {/* left side  */}
+          <div className="n-logo">
+            <span>Shaad Imran</span>
+          </div>
+
+          {/* right side  */}
+          <div className="n-right">
+            <div className="n-menu">
+              <span>What we do</span>
+              <span>How it works</span>
+              <span>Who we invest in</span>
+              <span>Testimonials</span>
+            </div>
+
+            <div className="fund-button">get funded</div>
+          </div>
+        </div>
+      </div>
+
+      {/* mobile/tab version  */}
+      <div className="nm-container">
+        {/* logo */}
+        <span>Shaad Imran</span>
+
+        {/* menu icon  */}
+        {!mobileMenuOpened ? (
+          <BiMenuAltRight size={30} onClick={() => setMobileMenuOpened(true)} />
+        ) : (
+          <RxCross2 size={30} onClick={() => setMobileMenuOpened(false)} />
+        )}
+
+        {/*  mobile menu */}
+        <div
+          className="nm-menu"
+          style={{ transform: mobileMenuOpened && "translateX(0%)" }}
+        >
+          <span>What we do</span>
+          <span>How it works</span>
+          <span>Who we invest in</span>
+          <span>Testimonials</span>
+
+          <div className="m-fund-button">get funded</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
